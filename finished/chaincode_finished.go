2fa172763522c4c9808c2b0da378cc6521e5deee
PiyushBhalerao <piyushbhalerao7@gmail.com>
2017-03-06 11:53:59 +0530

    demo

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,198 +1,177 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
 
-	"errors"
-	"fmt"
-
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/core/crypto/primitives"
+    "errors"
+    "fmt"
+    "encoding/json"
+    "github.com/hyperledger/fabric/core/chaincode/shim"
+	  "github.com/hyperledger/fabric/core/crypto/primitives"
 
 )
 
-
-
-// SimpleChaincode example simple Chaincode implementation
-type StudentManagementChaincode struct {
-}
-
-
-
-func main() {
-	primitives.SetSecurityLevel("SHA3", 256)
-	err := shim.Start(new(StudentManagementChaincode))
-	if err != nil {
-		fmt.Printf("Error starting AssetManagementChaincode: %s", err)
-	}
-}
-
-// Init resets all the things
-func (t *StudentManagementChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 0 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 0")
-	}
-
-	// create student imformation table
-
-	err := stub.CreateTable("StudentData", []*shim.ColumnDefinition{
-		&shim.ColumnDefinition{Name: "RollNumber", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "Name", Type: shim.ColumnDefinition_STRING, Key: false},
-		&shim.ColumnDefinition{Name: "Marks", Type: shim.ColumnDefinition_STRING, Key: false},
-	})
-
-	if err != nil{
-		return nil, errors.New("Unable to create student Data table")
-	}
-
-
-	return nil, nil
-}
-
-// Invoke isur entry point to invoke a chaincode function
-func (t *StudentManagementChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
-	// Handle different functions
-	if function == "init" {
-		//Initialize the StudentData
-		return t.Init(stub, "init", args)
-	} else if function == "insert_student" {
-		//insert new entry in StudentData
-		return t.insert_student(stub, "insert_student" , args)
-	} else if function == "delete_student"{
-		return t.delete_student(stub,"delete_student",args)
-	}
-	fmt.Println("invoke did not find func: " + function)
-
-	return nil, errors.New("Received unknown function invocation: " + function)
-}
-
-// Query is our entry point for queries
-func (t *StudentManagementChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
-
-	// Handle different functions
-	if function == "read" { //read a variable
-		return t.read_data(stub ,"read_data",args)
-	}
-	fmt.Println("query did not find func: " + function)
-
-	return nil, errors.New("Received unknown function query: " + function)
-}
-
-//insert new student
-func (t *StudentManagementChaincode) insert_student(stub shim.ChaincodeStubInterface , function string ,args []string)([]byte,error) {
-
-	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
-
-	var err error
-
-	RollNumber :=args[0]
-	Name := args[1]
-	Marks :=args[2]
-
-	_, err = stub.InsertRow(
-		"StudentData",
-		shim.Row{
-			Columns: []*shim.Column{
-				&shim.Column{Value: &shim.Column_String_{String_: RollNumber}},
-				&shim.Column{Value: &shim.Column_String_{String_: Name}},
-				&shim.Column{Value: &shim.Column_String_{String_: Marks}},
-			},
-	})
-		if err != nil {
-		return nil, errors.New("Failed inserting row.")
-	}
-
-fmt.Println("Succesfully added student");
-
-msg := "Succesfully added student"
-sendMsg := fmt.Sprintf("%s",msg)
-return []byte(sendMsg), nil
-
+//structure for HealthCareChaincode implementation
+type HealthCareChaincode struct {
 
 }
 
-//reading StudentData
-func (t *StudentManagementChaincode) read_data(stub shim.ChaincodeStubInterface , function string ,args []string)([]byte,error) {
+//Structure for Points(RewardPoint)
+type RewardPoint struct{
 
-	if(len(args) != 1){
-		return nil, errors.New("Wrong args..plz check")
-
-	}
-
-
-RollNumber := args[0]
-
-	var columns []shim.Column
-	col1 := shim.Column{Value: &shim.Column_String_{String_: RollNumber}}
-	columns = append(columns, col1)
-
-var err error
-	row, err := stub.GetRow("StudentData", columns)
-
-	if(err != nil){
-		return nil, errors.New("Failed inserting row.")
-	}
-	fmt.Println("Details are",row)
-
-	rowString := fmt.Sprintf("%s", row)
-	return []byte(rowString), nil
-
-
-
-
+	Points string `json:"Points"`
+	Hash string `json:"Hash"`
+	SignatureAssigner string `json:"SignatureAssigner"`
+  Tx_ID string `json:"Tx_ID_req"`
 
 }
-// delete particular student Entry from StudentData
-func (t *StudentManagementChaincode) delete_student(stub shim.ChaincodeStubInterface , function string ,args []string)([]byte,error) {
 
-	if(len(args)!=1){
-				return nil,errors.New("Wrong arguments")
-	}
-	delete_number := args[0]
+func main()  {
+  primitives.SetSecurityLevel("SHA3", 256)
+  err := shim.Start(new(HealthCareChaincode))
+  if err != nil {
+    fmt.Printf("Error starting AssetManagementChaincode: %s", err)
+  }
+}
 
-	var columns [] shim.Column
-	col1 := shim.Column{Value : &shim.Column_String_{String_:delete_number}}
-	columns = append(columns,col1)
+//Init function : Table-Creaction
+func (t *HealthCareChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string)([]byte, error){
+fmt.Println("In Init function ")
+  var err error
 
-	var err error
+  if(len(args)!=0){
+    return nil , errors.New("Incorrect number of arguments..Want 0 arguments");
+  }
 
-	 row,err := stub.GetRow("StudentData",columns)
-	if(err != nil){
-			return nil,errors.New("RollNumber doenNot exists")
-	}
+  //res := RewardPoint{}
 
-	err = stub.DeleteRow(
-		"StudentData",
-		[]shim.Column{shim.Column{Value: &shim.Column_String_{String_: delete_number}}},
-	)
+  // err = stub.PutState("UserID",res)
+  // if err != nil {
+	// 	return nil, err
+	// }
 
-	if(err !=nil){
-		return nil,errors.New("Wrong Details")
+  //Table Per User
+  err = stub.CreateTable("TransactionTable", []*shim.ColumnDefinition{
+    &shim.ColumnDefinition{Name :"Tx_ID",Type :shim.ColumnDefinition_STRING,Key :true},
+  //  &shim.ColumnDefinition{Name :"RewardPoint",Type : shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"From",Type :shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"To",Type :shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"Signature_receiver",Type :shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"Signature_assigner",Type :shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"TimeStamp",Type :shim.ColumnDefinition_STRING,Key :false},
+    &shim.ColumnDefinition{Name :"Reason",Type :shim.ColumnDefinition_STRING,Key :false},
+  })
 
-	}
+if err != nil {
+  return nil,errors.New("Failed to create table")
+}
 
-	Srow :=fmt.Sprintf("%s",row)
+
+  //Set adminCert
+  adminCert, err := stub.GetCallerMetadata()
+  if err!=nil{
+    return nil, errors.New("Not getting proper metadata")
+  }
+
+  if len(adminCert)== 0{
+    return nil,errors.New("Invalid Admin certificate:")
+  }
+
+stub.PutState("admin",adminCert)
+return nil,nil;
+}
 
 
-	return []byte(Srow),nil
+func(t *HealthCareChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string)([]byte, error) {
+  fmt.Println("In Invoke func")
+
+  if function =="init" {
+    return t.Init(stub,"init",args)
+  }else if(function=="AssignPoints"){
+    return t.AssignPoints(stub,"AssignPoints",args)
+  }else if(function=="RedeemPoints"){
+    return t.RedeemPoints(stub,"RedeemPoints",args)
+  }
+
+
+return nil,nil;
+
+}
+func (t *HealthCareChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error)  {
+  fmt.Println("Query function is running")
+
+  if function == "read"{
+    return t.read(stub ,"read",args)
+  }
+  fmt.Println("Error in Query"+ function)
+return nil,nil
+}
+
+func (t *HealthCareChaincode) read(stub shim.ChaincodeStubInterface, function string , args []string)([]byte ,error){
+  var name ,jsonResp string
+  var err error
+
+  if len(args) != 1{
+      return nil,errors.New("Incorrect number of arguments")
+  }
+  name = args[0]
+  valAsBytes, err :=stub.GetState(name)
+
+  if err != nil{
+    jsonResp ="{\"Error\":\"Failed to get state for "+ name + "\"}"
+    return nil,errors.New(jsonResp)
+  }
+
+  return valAsBytes , nil
+
+}
+
+func (t *HealthCareChaincode) AssignPoints(stub shim.ChaincodeStubInterface , function string, args []string)([]byte,error)  {
+
+    var err error
+
+    if len(args) !=3 {
+          return nil,errors.New("Incorrect numbers of arguments")
+    }
+
+
+   name := args[0]
+   value, err := stub.GetState(name)
+
+   if(err !=nil){
+      return  t.init_eReward(stub,"eReward",args)
+   }else{
+
+     var inputPoints = args[1]
+     var inputAssigner = args[2]
+
+     res := RewardPoint{}
+     json.Unmarshal(value , &res)
+
+
+     res.Points = (res.Points + inputPoints)
+     res.SignatureAssigner = inputAssigner
+
+     jsonAsBytes, _ := json.Marshal(res)
+     err = stub.PutState(name , jsonAsBytes)
+
+     if err != nil {
+       return nil, err
+     }
+   }
+   successMsg := fmt.Sprintf("%s",value)
+return []byte(successMsg),nil
+}
+
+func (t *HealthCareChaincode) RedeemPoints(stub shim.ChaincodeStubInterface , function string, args []string)([]byte,error)  {
+return nil,nil
+}
+
+func (t *HealthCareChaincode) init_eReward(stub shim.ChaincodeStubInterface,function string, args []string)([]byte,error) {
+
+  if len(args) != 2 {
+    return nil,errors.New("Incorrect number of arguments")
+  }
+
+  return nil,nil;
+
 }
